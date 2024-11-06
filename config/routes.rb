3b5965beef8b0bfae6d625d732715498d582b009
sthomas315 <147417972+sthomas315@users.noreply.github.com>
2024-11-06 21:45:21 +0000

    fixed courses

--- config/routes.rb
+++ config/routes.rb
@@ -21,10 +21,10 @@ Rails.application.routes.draw do
 
   get("/courses", { :controller => "courses", :action => "index" })
  
-  get("/insert_course", { :controller => "courses", :action => "create" })
+  post("/insert_course", { :controller => "courses", :action => "create" })
   get("/courses", { :controller => "courses", :action => "index" })
   get("/courses/:path_id", { :controller => "courses", :action => "show" })
-  get("/modify_course/:path_id", { :controller => "courses", :action => "update" })
+  post("/modify_course/:path_id", { :controller => "courses", :action => "update" })
 
   get("/delete_course/:path_id", { :controller => "courses", :action => "destroy" })
 
